e4e57a979065b1e9a39721d1696f15bce3676877
Joao Habib da Silva <contajh544@gmail.com>
2025-07-23 17:34:16 -0300

    transitions and animations done

--- assets/Js/index.js
+++ assets/Js/index.js
@@ -4,3 +4,23 @@ for (var i = 0; i < 72; i++) {
     element_created.classList.add("container-background-circles-each")
     box.appendChild(element_created)
 }
+
+
+window.onload = function () {
+  var image = window.document.getElementById("center");
+  image.classList.add("start");
+  var triangle = window.document.getElementById("container-background-triangle");
+    triangle.classList.add("start")
+    var retangle1 = window.document.getElementById("retangle1")
+    retangle1.classList.add("start")
+    var retangle2 = window.document.getElementById("retangle2")
+    retangle2.classList.add("start")
+    var retangle3 = window.document.getElementById("retangle3")
+    retangle3.classList.add("start")
+    var retangle4 = window.document.getElementById("retangle4")
+    retangle4.classList.add("start")
+    var retangle5 = window.document.getElementById("retangle5")
+    retangle5.classList.add("start")
+    var retangle6 = window.document.getElementById("retangle6")
+    retangle6.classList.add("start")
+};
